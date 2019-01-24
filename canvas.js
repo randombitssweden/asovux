e1450e20b1345bff6e891dabb164e3d378d57b7e
Peter Roosdorp <peter.roosdorp@randombits.se>
2019-01-24 14:26:30 +0100

    Update canvas

--- canvas.js
+++ canvas.js
@@ -4,9 +4,19 @@ rityta.width=500;
 rityta.height=500;
 rityta.style.border="solid 1px #1E90FF";
 c.beginPath();
-for (var i = 0; i < 10 ; i++) {
-    console.log(0+i)
-    c.moveTo(0+i,0+i);
-    c.lineTo(500+i,500+i);
+for (var i = -500; i < 500 ; i+=20) {
+    c.moveTo(i,0);
+    c.lineTo(i+500,rityta.height);
+    c.closePath();
     c.stroke();    
 }
+for(var j = 0; j < 1000; j+=20)
+{
+    c.beginPath();
+    c.moveTo(j,0);
+    c.lineTo(j-500,rityta.height);
+    c.closePath();
+    c.stroke();
+}
+
+
