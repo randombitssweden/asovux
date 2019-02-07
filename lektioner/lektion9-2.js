8ff278222ac3e687f702ac6cc501ff811cd9eef8
Peter Roosdorp <peter.roosdorp@randombits.se>
2019-02-07 14:24:14 +0100

    Not working

--- lektioner/lektion9-2.js
+++ lektioner/lektion9-2.js
@@ -7,8 +7,17 @@ var ctx = canvas.getContext("2d");
 for (var i = 0; i < 500; i+10) {
     ctx.beginPath();
     ctx.moveTo(500-i,500);
-    ctx.lineTo(0.500-i);
+    ctx.lineTo(0,500-i);
     ctx.strokeStyle = "blue";
     ctx.closePath();
     ctx.stroke();
+}
+
+for (var e = 0; e < 21 ; e++) {
+    ctx.beginPath();
+    ctx.ellipse(250,250,a,b,0,0,2*Math.PI);
+    ctx.closePath();
+    ctx.stroke();
+    a += 10 
+    b -= 10;
 }
