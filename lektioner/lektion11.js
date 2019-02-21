99bae72f847008bef9f60d5db9876f2b9bc80b13
Peter Roosdorp <peter.roosdorp@randombits.se>
2019-02-21 13:41:07 +0100

    Working example

--- lektioner/lektion11.js
+++ lektioner/lektion11.js
@@ -5,17 +5,30 @@ var ctx = canvas.getContext("2d");
 canvas.style.border = "solid 1px black";
 
 function kvadrat(color) {
-    ctx.rect(100,100,20,20);
+    ctx.rect(250,150,100,100);
     ctx.fillStyle = color;
     ctx.fill();
 }
 
 kvadrat("red");
 
-canvas.addEventListener("click", function() {
-    var x = event.clientX;
-    var y = event.clientY;
-    if(x >= 100 && x <= 120 && y >= 100 && y <= 120) {
+function getMousePos(canvas, evt) {
+    var rityta = canvas.getBoundingClientRect();
+    return {
+    x: evt.clientX - rityta.left,
+    y: evt.clientY - rityta.top
+    };
+}
+
+canvas.addEventListener("mousemove", function() {
+    //var x = event.clientX;
+    //var y = event.clientY;
+    var pos = getMousePos(canvas,event)
+    //document.getElementById("pos").innerHTML += "X: "+x+" och Y: "+y;
+    //document.getElementById("pos").innerHTML += "<br>pos.x " +pos.x+ "pos.y "+pos.y;
+    if(pos.x >= 250 && pos.x <= 350 && pos.y >= 150 && pos.y <= 350) {
         kvadrat("green");
+    } else {
+        kvadrat("red")
     }
 });
