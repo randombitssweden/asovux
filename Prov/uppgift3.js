0e2450e116c2c828428c81b81f4e4e6f5266c9d3
Peter Roosdorp <peter.roosdorp@randombits.se>
2019-03-05 15:57:01 +0100

    Done for now.

--- Prov/uppgift3.js
+++ Prov/uppgift3.js
@@ -1,21 +1,44 @@
-var canvas = document.getElementById("minCanvas");
+var canvas = document.getElementById("canvas");
 canvas.width = 600;
 canvas.height = 400;
-var context = canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 canvas.style.border = "solid 1px black";
 
+ctx.lineWidth = 3;
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+//ctx.strokeStyle="red";
+
 function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
+    var rityta = canvas.getBoundingClientRect()
     return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
+    x: evt.clientX - rityta.left,
+    y: evt.clientY - rityta.top
     };
+   }
+   
+
+function randomInt(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+};
+
+function randomColor() {
+    var colorint = randomInt(10,60);
+    if (colorint < 20) return "blue";
+    if (colorint < 30 && colorint > 19) return "green";
+    if (colorint < 40 && colorint > 29) return "pink";
+    if (colorint < 50 && colorint > 39) return "purple";
+    if (colorint < 60 && colorint > 49) return "red";
 }
-function drawCircle(size, x, y, colour) { //draw circle
-    var size = Math.floor(Math.random() * 100);
-    //var colour = '#' + Math.random().toString(16).substr(2,6); // random colour;
+canvas.addEventListener("click",function(evt){
+    var pos = getMousePos(canvas, evt);
+    var size = randomInt(10,30);
+    var color = randomColor();
     ctx.beginPath();
-    ctx.arc(x, y, size, 0, 2 * Math.PI);
-    ctx.fillStyle = colour;
+    ctx.arc(pos.x, pos.y, size, 0, 2 * Math.PI);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.fillStyle = color;
     ctx.fill();
-}
+   },true);
+
